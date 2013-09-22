00c99a47b1ff0fc61782079b59fa48dac1e4e6f3
Paul Worthington <pworthington@regis.edu>
2013-09-22 17:31:57 -0600

    Added Service layer, repository factory, and started Service layer unit test

---
+++ Service/RFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DAL;
+
+namespace Service
+{
+
+    // sample code
+    public class RFactory
+    {
+        public IGenericRepository GetRepository(string sRepositoryType)
+        {
+            IGenericRepository objRepo;
+            switch (sRepositoryType)
+            {
+                case "Item":
+                    objRepo = new GenericRepository<Item>();
+                    break;
+                case "ItemDescription":
+                    objRepo = new GenericRepository<ItemDescription>();
+                    break;
+                default:
+                    objRepo = null;
+                    throw new System.ArgumentException("Unimplemented Repository type " + sRepositoryType);
+            }
+            return objRepo;
+        }
+    }
+
+
+
+}
---
+++ ServiceLayerUnitTest/ServiceUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+//using System.Data;
+using System.Linq;
+using DAL;
+using Service;
+
+namespace ServiceLayerUnitTest
+{
+    [TestClass]
+    public class ServiceUnitTest
+    {
+
+
+        [TestMethod]
+        public void UseRepositoryFactoryToAddItem()
+        {
+            /*
+             * Create an RFactory object
+             * Use the factory to create an Item repository object
+             * Create an Item object
+             * Use the Item repository to add the Item
+             */
+
+            RFactory myFactory = new RFactory();
+            var itemRepository = myFactory.GetRepository("Item");
+
+            Item myItem = new Item();
+            myItem.name = "half-coin";
+            myItem.type = 2;
+            myItem.firstMentionBook = 0;
+            myItem.firstMentionChapter = 28;
+
+            itemRepository.Insert(myItem);
+
+        }
+    }
+}
