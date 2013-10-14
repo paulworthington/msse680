d25e65214959844609469a429dfd701e8ebdd39a
Paul Worthington <pworthington@regis.edu>
2013-10-14 01:09:46 -0600

    WHY does Visual Studio sometimes not track files?!

---
+++ Presentation/ItemAddForm.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Business;
+using DAL;
+
+namespace Presentation
+{
+    public partial class ItemForm : System.Web.UI.Page
+    {
+        /*
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+         */
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            // instantiate an Item object
+            // populate the Item object from the text boxes
+            // instantiate an ItemManager object
+            // call the ItemManager's Insert method and pass the Item
+
+            Item myItem = new Item();
+
+            myItem.name = this.TextBox1.Text;
+            myItem.type = this.DropDownList1.SelectedIndex;
+            myItem.firstMentionBook = this.DropDownList2.SelectedIndex;
+            myItem.firstMentionChapter = Convert.ToInt32(this.TextBox4.Text);
+
+            ItemMgr itemManager = new ItemMgr();
+            itemManager.Insert(myItem);
+            System.Web.UI.ScriptManager.RegisterStartupScript(this, typeof(Page), "alert", "alert('" + "Item added" + "')", true);
+        }
+
+    }
+}
